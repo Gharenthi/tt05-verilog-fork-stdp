1eb6248ae89ad5ca7e848da91f9cd3a83983fe83
WBodeau <wbodeau@gmail.com>
2023-11-03 23:43:16 -0700

    Cheeeeet

--- src/synapse.v
+++ src/synapse.v
@@ -9,9 +9,11 @@ module synapse(
 
 assign activation = 2'b00000000;
 
+// wire [0:7] rw_out;
+
 //reg8 r_pre_hyst();
 //reg8 r_post_hyst();
-//reg8 r_weight();
+// reg8 r_weight(.write(sum_3[0:7]), .read(rw_out[0:7]), .clk(clk), .reset(reset));
 
 //add8 post_mult(.in_a(), .in_b(), .sum() .carry_in(), .carry_out())
 
